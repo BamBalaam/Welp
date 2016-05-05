b8727d67b288117e32b0b34617863a51ae816d20
BamBalaam <amadeiracortes@gmail.com>
2016-05-05 14:16:27 +0200

    Modified creation script slightly

--- src/creation.sql
+++ src/creation.sql
@@ -1,57 +1,68 @@
 CREATE TABLE "User"(
-	UserID			Integer,
-	Email			Varchar(100),
-	"Password"		Varchar(64),
-	DateSignUp		Varchar(100),
-	IsAdmin			Boolean,
+	UserID			Serial,
+	Email			Varchar(100) NOT NULL,
+	"Password"		Varchar(64) NOT NULL,
+	DateSignUp		Varchar(100) NOT NULL,
+	IsAdmin			Boolean DEFAULT False,
 	PRIMARY KEY (UserID)
 );
 
 CREATE TABLE Place(
-	PlaceID			Integer,
-	Name			Varchar(100),
-	Address			Varchar(64),
-	GPS				Float,
+	PlaceID			Serial,
+    CreatorID       Serial REFERENCES "User"(UserID),
+    CreationDate	Timestamp,
+	Name			Varchar(100) NOT NULL,
+	Street			Varchar(64) NOT NULL,
+	Num				Varchar(10) NOT NULL,
+	Zip				Integer NOT NULL,
+	City			Varchar(100) NOT NULL,
+	Longitude		Float,
+	Latitude		Float,
 	PhoneNum		Varchar(100),
 	Website			Varchar(100),
 	PRIMARY KEY (PlaceID)
 );
 
 CREATE TABLE Restaurant(
-	PID			Integer,
+	PID			Serial REFERENCES Place(PlaceID),
 	PriceRange	Varchar(200),
 	Banquet		Integer,
 	TakeOut		Boolean,
 	Delivery	Boolean,
-	Closed		Varchar(300),
-	FOREIGN KEY (PID) REFERENCES Place(PlaceID)
+	Closed		Varchar(300)
 );
 
 CREATE TABLE Cafe(
-	PID			Integer,
+    PID         Serial REFERENCES Place(PlaceID),
 	Smoking		Boolean,
-	Snack		Boolean,
-	FOREIGN KEY (PID) REFERENCES Place(PlaceID)
+	Snack		Boolean
 );
 
 CREATE TABLE Hotel(
-	PID			Integer,
+	PID			Serial REFERENCES Place(PlaceID),
 	NumStars	Integer,
 	NumRooms	Integer,
-	PriceRangeDoubleRoom	Varchar(200),
-	FOREIGN KEY (PID) REFERENCES Place(PlaceID)
+	PriceRangeDoubleRoom	Varchar(200)
 );
 
 CREATE TABLE "Comment"(
-	PID 			Integer,
-	UID				Integer,
-	"Comment" 		Text,
+	PID 			Serial REFERENCES Place(PlaceID),
+	UID				Serial REFERENCES "User"(UserID),
+	"Text" 			Text,
 	"Date" 			Timestamp,
-	FOREIGN KEY (PID) REFERENCES Place(PlaceID),
-	FOREIGN KEY (UID) REFERENCES "User"(UserID),
-	PRIMARY KEY (PID, UID)
+	PRIMARY KEY (PID, UID,"Date")
 );
 
 CREATE TABLE Tag(
-	Name Varchar(100)
+	PID 			Serial REFERENCES Place(PlaceID),
+	UID				Serial REFERENCES "User"(UserID),
+	Name			Varchar(100),
+	PRIMARY KEY (PID, UID)
+);
+
+CREATE TABLE Creates(
+	PID 			Serial REFERENCES Place(PlaceID),
+	UID				Serial REFERENCES "User"(UserID),
+	CreationDate	Timestamp,
+	PRIMARY KEY (PID, UID)
 );
